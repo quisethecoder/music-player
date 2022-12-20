2fcef5044cc42b6e3b98456dbac55d88848c65ff
quisethecoder <marquise_dawkins@aol.com>
2022-12-19 20:07:31 -0500

    button html connect

--- index.js
+++ index.js
@@ -2,3 +2,8 @@ let now_playing = document.querySelector(".now-playing");
 let track_art = document.querySelector(".track-art");
 let track_name = document.querySelector("track_name");
 let track_artist = document.querySelector(".track-artist");
+
+let playpause_btn = document.querySelector(".playpause-track");
+let next_btn = documeny.querySelector(".next-track");
+let prev_btn = document.querySelector(".prev_track");
+
