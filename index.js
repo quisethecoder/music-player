494b22bf72df44a7de46be6ee2c52f9597e1823b
quisethecoder <marquise_dawkins@aol.com>
2022-12-19 22:57:54 -0500

    loadTrack function

--- index.js
+++ index.js
@@ -72,3 +72,22 @@ const music_list = [
 ];
 
 loadTrack(track_index);
+
+function loadTrack(track_index){
+    clearInterval(updateTimer)
+    resizeTo();
+
+    curr_track.src = music_list[track_index].music;
+    curr_track.load();
+
+    track_art.getElementsByClassName.backgroundImage = "url(" + music_list[track_index].img + ")";
+    track_name.textContent = music_list[track_index].name;
+    track_artist.textContent = music_list[track_index].artist;
+    now_playing.textContent = "Playing music " + (track_index + 1) + " of " + music_list.length;
+
+    updateTimer = setInterval(setUpdate, 1000);
+
+    curr_track.addEventListener("ended", nextTrack);
+    random_bg_color();
+}
+
