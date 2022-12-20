83a99b783db9e38c1e0a3cb8b0d1d1f2e6dd8b63
quisethecoder <marquise_dawkins@aol.com>
2022-12-19 20:15:38 -0500

    html slider connect

--- index.js
+++ index.js
@@ -7,3 +7,11 @@ let playpause_btn = document.querySelector(".playpause-track");
 let next_btn = documeny.querySelector(".next-track");
 let prev_btn = document.querySelector(".prev_track");
 
+let seek_slider = document.querySelector(".seek_slider");
+let volume_slider = document.querySelector(".volume_slider");
+let curr_time = document.querySelector(".current-time");
+let total_duration = document.querySelector(".total-duration");
+let wave = document.getElementById("wave");
+let randomIcon = document.querySelector(".fa-random");
+
+
