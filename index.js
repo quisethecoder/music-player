84e069d12af0635cf1e89a6138bb3c88c76a6add
quisethecoder <marquise_dawkins@aol.com>
2022-12-20 22:54:36 -0500

    final touches

--- index.js
+++ index.js
@@ -170,8 +170,8 @@ function nextTrack(){
 }
 
 function prevTrack(){
-    if(track > 0){
-        track -= 1;
+    if(track_index > 0){
+        track_index -= 1;
     }else{
         track_index = music_list.length -1;
     }
@@ -186,4 +186,25 @@ function seekTo(){
 
 function setVolume(){
     curr_track.volume = volume_slider.value / 100;
+}
+
+function setUpdate(){
+    let seekPosition = 0;
+    if(!isNaN(curr_track.duration)){
+        seekPosition = curr_track.currentTime * (100 / curr_track.duration);
+        seek_slider.value = seekPosition;
+
+        let currentMinutes = Math.floor(curr_track.currentTime / 60);
+        let currentSeconds = Math.floor(curr_track.currentTime - currentMinutes * 60);
+        let durationMinutes = Math.floor(curr_track.duration / 60);
+        let durationSeconds = Math.floor(curr_track.duration - durationMinutes * 60);
+
+        if(currentSeconds < 10){currentSeconds = "0" + currentSeconds; }
+        if(durationSeconds < 10){durationSeconds = "0" + durationSeconds; }
+        if(currentMinutes < 10){currentMinutes = "0" + currentMinutes; }
+        if(durationMinutes < 10){durationMinutes = "0" + durationMinutes; }
+
+        curr_time.textContent = currentMinutes + ":" + currentSeconds;
+        total_duration.textContent = durationMinutes + ":" + durationMinutes;
+    }
 }
